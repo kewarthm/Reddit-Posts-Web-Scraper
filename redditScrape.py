0799818e57978c28dd84304d9a287c22b8606d59
Matthew Kewarth <mattkewarth@hotmail.com>
2022-07-27 19:37:52 -0400

    Update redditScrape.py
    Change return value to a 2D dictionary for ease of use and clarity

--- redditScrape.py
+++ redditScrape.py
@@ -9,54 +9,62 @@ options = webdriver.ChromeOptions()
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
 fileDir = os.path.dirname(os.path.realpath('__file__'))
 path = os.path.join(fileDir, "chromedriver.exe")
-sample_site = "https://www.reddit.com/r/manga"
-sample_targets = ["Good Morning", "Please Go Home", "Jujutsu Kaisen", "Ganbare", "Dandadan", "RuriDragon", "One Punch Man", "Getting less and less tsun"]
 
-
-def reddit_search(site, targets):
+def reddit_search(subreddit, targets):
     '''
-        (str, [str]) -> [[str, str]]
+        (str, [str]) -> {str : {str : str}}
         Takes a subreddit name and a list of keywords. Using Selenium and Beautiful soup
         all recent posts with titles containing a keyword will be returned.
 
-        site: the name of the target subreddit
+        subreddit: the name of the target subreddit
         targets: a list of keywords to look for in posts
-        Return: a list of pairs of the form [Post title, link to post]
+        Return: a 2D dict [x][y] where:
+            x is a keyword
+            y is a title of a post found to contain keyword x
+            dict[x][y] contains the link to the post
 
     '''
+    results = {}
+    for t in targets:
+        results[t] = {}
+
     driver = webdriver.Chrome(options=options, executable_path = path)
-    driver.get(site)
+    driver.get("https://www.reddit.com/r/" + subreddit)
     driver.implicitly_wait(220)
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight,)")
     page = driver.page_source
     driver.close()
+
     soup = BeautifulSoup(page, "html.parser")
     posts = soup.select('[data-testid="post-container"]')
-    res = []
-    link_res = []
+
+    if not posts:
+        raise Exception("Subreddit does not exist")
+
     for p in posts:
         title = p.select_one('h3').text
         for t in targets:
             if t.lower() in title.lower():
                 if p.select_one('figure') is not None:
                     #post has an in-laid image(s), save href to post
-                    res.append(title)
-                    link_res.append("https://reddit.com" + p.select_one('[data-click-id="body"]')['href'])
+                    results[t][title] = "https://reddit.com" + p.select_one('[data-click-id="body"]')['href']
 
                 elif p.select_one('[data-click-id="media"]') is not None:
-                    res.append(title)
-                    link_res.append("https://reddit.com" + p.select_one('[data-click-id="body"]')['href'])
+                    results[t][title] = "https://reddit.com" + p.select_one('[data-click-id="body"]')['href']
 
                 else:
-                    res.append(title)
-                    link_res.append(p.select_one('[data-testid="outbound-link"]')['href'])
+                    results[t][title] = p.select_one('[data-testid="outbound-link"]')['href']
+
                 break
-    return [res, link_res]
+    return results
 
 if __name__ == "__main__":
+    sample_site = "manga"
+    sample_targets = ["Good Morning", "Please Go Home", "Jujutsu Kaisen", "Ganbare", "Dandadan", "RuriDragon", "One Punch Man", "Getting more and more"]
     r = reddit_search(sample_site, sample_targets)
 
     print("--- Results ---")
-    for i in range(len(r)):
-        print(r[0][i])
-        print(r[1][i])
+    for k in r.keys():
+        print("-" + k)
+        for p in r[k].keys():
+            print("\t" + p + " : " + r[k][p])
